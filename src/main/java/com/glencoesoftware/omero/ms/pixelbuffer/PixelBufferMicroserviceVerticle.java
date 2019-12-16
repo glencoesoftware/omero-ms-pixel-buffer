a1eb46629919a3b65d4f9132ea56b3b3eb550341
Kevin Kozlowski <kevin@glencoesoftware.com>
2019-12-16 13:07:21 -0700

    Change tracing configuration to allow full disabling

--- src/main/java/com/glencoesoftware/omero/ms/pixelbuffer/PixelBufferMicroserviceVerticle.java
+++ src/main/java/com/glencoesoftware/omero/ms/pixelbuffer/PixelBufferMicroserviceVerticle.java
@@ -21,6 +21,7 @@ package com.glencoesoftware.omero.ms.pixelbuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -165,26 +166,38 @@ public class PixelBufferMicroserviceVerticle extends OmeroMsAbstractVerticle {
                 httpTracingConfig.getBoolean("enabled", false);
         if (tracingEnabled) {
             String zipkinUrl = httpTracingConfig.getString("zipkin-url");
-            log.info("Tracing enabled: {}", zipkinUrl);
-            sender = OkHttpSender.create(zipkinUrl);
-            spanReporter = AsyncReporter.create(sender);
-            PrometheusSpanHandler prometheusSpanHandler = new PrometheusSpanHandler();
-            tracing = Tracing.newBuilder()
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .localServiceName("omero-ms-pixel-buffer")
-                .addFinishedSpanHandler(prometheusSpanHandler)
-                .spanReporter(spanReporter)
-                .build();
+            try {
+                log.info("Tracing enabled: {}", zipkinUrl);
+                if(Pattern.matches("^http.*", zipkinUrl)) {
+                    sender = OkHttpSender.create(zipkinUrl);
+                    spanReporter = AsyncReporter.create(sender);
+                    PrometheusSpanHandler prometheusSpanHandler = new PrometheusSpanHandler();
+                    tracing = Tracing.newBuilder()
+                        .sampler(Sampler.ALWAYS_SAMPLE)
+                        .localServiceName("omero-ms-pixel-buffer")
+                        .addFinishedSpanHandler(prometheusSpanHandler)
+                        .spanReporter(spanReporter)
+                        .build();
+                } else if (Pattern.matches("^slf4j.*", zipkinUrl)) {
+                    PrometheusSpanHandler prometheusSpanHandler = new PrometheusSpanHandler();
+                    spanReporter = new LogSpanReporter();
+                    tracing = Tracing.newBuilder()
+                            .sampler(Sampler.ALWAYS_SAMPLE)
+                            .localServiceName("omero-ms-pixel-buffer")
+                            .addFinishedSpanHandler(prometheusSpanHandler)
+                            .spanReporter(spanReporter)
+                            .build();
+                } else {
+                    throw new IllegalArgumentException("Invalid URL configured for tracing");
+                }
+            } catch (Exception e) {
+                log.error("Tracing enabled but configured incorrectly");
+                throw e;
+            }
         } else {
             log.info("Tracing disabled");
-            PrometheusSpanHandler prometheusSpanHandler = new PrometheusSpanHandler();
-            spanReporter = new LogSpanReporter();
-            tracing = Tracing.newBuilder()
-                    .sampler(Sampler.ALWAYS_SAMPLE)
-                    .localServiceName("omero-ms-pixel-buffer")
-                    .addFinishedSpanHandler(prometheusSpanHandler)
-                    .spanReporter(spanReporter)
-                    .build();
+            tracing = Tracing.newBuilder().build();
+            tracing.setNoop(true);
         }
         httpTracing = HttpTracing.newBuilder(tracing).build();
 
